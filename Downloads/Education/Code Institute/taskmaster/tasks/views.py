7f0c1e0c9b1df7d17ded83bbe3ebb82bd35f999a
b0bengo <jsobeng@outlook.com>
2025-02-27 19:09:03 +0000

    Added views and DTL to html

--- Downloads/Education/Code Institute/taskmaster/tasks/views.py
+++ Downloads/Education/Code Institute/taskmaster/tasks/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Task
 
 # Create your views here.
+
+def home(request):
+    """
+    A view to display tasks to do and completed tasks with the tasks dues earliest at the top
+    """
+
+    to_do_tasks = Task.objects.filter(completed=False).order_by('due_date')
+    done_tasks = Task.objects.filter(completed=True).order_by('due_date')
+
+    context = {
+        'to_do_tasks': to_do_tasks,
+        'done_tasks': done_tasks,
+    }
+
+    return render(request, 'tasks/index.html', context)
